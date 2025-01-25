d28b13265d96eb0fa80e22691b12865da1a70d91
sameer240704 <sameergupta79711@gmail.com>
2025-01-25 18:55:22 +0530

    Added

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
+import os
 from app import create_app
 
 application = create_app()
 
 if __name__ == '__main__':
-    application.run()
+    application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
